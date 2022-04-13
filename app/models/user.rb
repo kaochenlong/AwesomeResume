7cab6b157ada63944d094a2f544fdea0f853f0c7
Eddie <eddie@5xcampus.com>
2022-04-13 14:34:05 +0800

    add user sign up

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,30 @@
 class User < ApplicationRecord
   validates :email, presence: true, uniqueness: true
   validates :username, presence: true, uniqueness: true
-  validates :password, presence: true
+  validates :password, presence: true,
+                       confirmation: true,
+                       length: { minimum: 6}
+
+  before_save :encrypt_password
+
+  private
+    def encrypt_password
+      self.password = encode_password(self.password)
+    end
+
+    def encode_password(pw)
+      Digest::SHA1.hexdigest(salted_string(pw))
+    end
+
+    def salted_string(str)
+      stuffing_chars = ["x", "y", "j", "a", "k", "z"]
+
+      part_a = str.chars[0..5].map.with_index { |v, i|
+        v + stuffing_chars[i]
+      }.join
+
+      part_b = str.chars[6..].join
+
+      "z#{part_a}#{part_b}y"
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   resources :resumes
 
-  get "/users/sign_up", to: "users#new"
+  resource :users, only: [:create] do
+    get :sign_up
+  end
+
+  # get "/users/sign_up", to: "users#new"
 
   post "/", to: "pages#home"
 
