27fb5cec758b903ced656820b1082d4bd1aeb2f3
Eddie <eddie@5xcampus.com>
2022-04-19 11:51:25 +0800

    prepare to use stimulus

--- app/controllers/resumes_controller.rb
+++ app/controllers/resumes_controller.rb
@@ -4,6 +4,7 @@ class ResumesController < ApplicationController
   before_action :authenticate_user, except: [:index, :show]
 
   def index
+    flash[:notice] = "123"
     @resumes = Resume.published
   end
 
