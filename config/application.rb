eb338e4e40847e0cce453318b979fb13e6d0e1bf
Eddie <eddie@5xcampus.com>
2022-04-22 20:28:39 +0800

    remove unused gems

--- config/application.rb
+++ config/application.rb
@@ -2,7 +2,21 @@
 
 require_relative 'boot'
 
-require 'rails/all'
+# require 'rails/all'
+require 'rails'
+# Pick the frameworks you want:
+require 'active_model/railtie'
+require 'active_job/railtie'
+require 'active_record/railtie'
+require 'active_storage/engine'
+require 'action_controller/railtie'
+require 'action_mailer/railtie'
+# require 'action_mailbox/engine'
+require 'action_text/engine'
+require 'action_view/railtie'
+require 'action_cable/engine'
+# require "sprockets/railtie" # This removes sprockets
+# require 'rails/test_unit/railtie'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -10,17 +24,22 @@ Bundler.require(*Rails.groups)
 
 module MyResume
   class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
     config.autoload_paths << "#{root}/app/lib"
 
     config.active_storage.variant_processor = :vips
+
+    config.generators do |g|
+      g.stylesheets     false
+      g.helper          false
+    end
+
+    config.time_zone = 'Taipei'
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
     #
-    # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
   end
 end
